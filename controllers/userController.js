6327aa8ee71422a650e8f6ced66f6365e2007c8d
Jake Browning <jacobbrowning90@gmail.com>
2024-02-22 19:38:36 +0000

    Add basic user form

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,3 +2,39 @@ const User = require('../models/user');
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 
+exports.sign_up_get = asyncHandler(async(req, res, next) => {
+    res.render('sign-up', { title: 'Sign-up' });
+});
+
+exports.sign_up_post = [
+    body("first_name")
+        .trim(),
+    body("last_name")
+        .trim(),
+    body("email")
+        .trim(),
+    body("password")
+        .trim(),
+
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+        const user = new User({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+            password: req.body.password,
+        });
+        if (!errors.isEmpty()) {
+            res.render('sign-up', {
+                title: 'Sign-up' ,
+                errors: errors.array(),
+            });
+            return;
+        } else {
+            await user.save();
+            //Placeholder console log
+            console.log('New user saved')
+            res.redirect('/');
+        }
+    })
+];
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+const user_controller = require('../controllers/userController');
+
 // Placeholder sign-up form
-router.get('/sign-up', function(req, res, next) {
-  res.render('sign-up', { title: 'Sign-up' });
-});
+// router.get('/sign-up', function(req, res, next) {
+//   res.render('sign-up', { title: 'Sign-up' });
+// });
+
+router.get('/sign-up', user_controller.sign_up_get);
 
 // TODO: create new user
-router.post('/sign-up', function(req, res, next) {
-  res.redirect('/');
-});
+// router.post('/sign-up', function(req, res, next) {
+//   res.redirect('/');
+// });
+
+router.post('/sign-up', user_controller.sign_up_post);
 
 // TODO: log-in form
 router.get('/log-in', function(req, res, next) {
